6aba14ef41f15fc2450618f73da7b4a351b9b289
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-29 17:15:37 -0500

    queryset

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'coreapi',
     'tasks',
 ]
 
@@ -128,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS authentication
 CORS_ALLOWED_ORIGINS = []
+
+# In settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
 from .views import TaskView
 
 # api versioning
@@ -8,4 +9,5 @@ router.register(r'tasks', TaskView, 'task')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title='TASKS Django CRUD API')),
 ]
