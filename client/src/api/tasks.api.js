5ceb2248a03158c0ac918eb89c1a291a86b59087
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2023-07-29 22:11:28 -0500

    Actualizar tareas

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -5,7 +5,10 @@ const taskApi = axios.create({
 
 
 export const getAllTasks = () => { return taskApi.get('/'); }
+export const getTask = (id) => { return taskApi.get(`/${id}/`); }
 
 export const createTask = (task) => { return taskApi.post('/', task); }
 
 export const deleteTask = (id) => { return taskApi.delete(`/${id}`); }
+
+export const updateTask = (id, task) => { return taskApi.put(`/${id}/`, task); }
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,16 +1,32 @@
-import { useForm } from 'react-hook-form';
-import { createTask, deleteTask } from '../api/tasks.api';
+import { useEffect } from 'react';
+import { get, useForm } from 'react-hook-form';
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api';
 import { useNavigate, useParams } from 'react-router-dom';
 
 export function TaskFormPage() {
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm()
   const params = useParams();
   const navigate = useNavigate();
 
   const onSubmit = handleSubmit(async data => {
-    await createTask(data);
+    if (params.id) {
+      await updateTask(params.id, data);
+    } else {
+      await createTask(data);
+    }
     navigate('/tasks');
   })
+  useEffect(() => {
+    async function loadTask() {
+      if (params.id) {
+        console.log('get task', params.id);
+        const res = await getTask(params.id)
+        setValue('title', res.data.title);
+        setValue('description', res.data.description);
+      }
+    }
+    loadTask();
+  }, [])
 
   return (
     <div>
